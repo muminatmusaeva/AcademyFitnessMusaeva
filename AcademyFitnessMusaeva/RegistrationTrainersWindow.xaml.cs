fe52b3fead8d223580737ac3c115d2a9eba7efb5
Муминат Мусаева <musaeva.m8319@fgbouvodgu.onmicrosoft.com>
2022-02-02 23:16:51 +0300

    Commit

---
+++ AcademyFitnessMusaeva/RegistrationTrainersWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AcademyFitnessMusaeva
+{
+    /// <summary>
+    /// Логика взаимодействия для RegistrationTrainersWindow.xaml
+    /// </summary>
+    public partial class RegistrationTrainersWindow : Window
+    {
+        public RegistrationTrainersWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Reg_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Registr_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Window_Auth_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
